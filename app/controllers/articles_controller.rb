2354c4f8d6b69866efab24ef0cce6fbef952e94c
chao-xian <dev@kelvingan.co.uk>
2014-08-30 13:04:14 +0100

    Validations with error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
   end
 
   def index
@@ -11,9 +12,12 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
 
-    redirect_to @article
   end
 
   def show
