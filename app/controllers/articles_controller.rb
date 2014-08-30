99159d21ca721ed825b9d6a7318df3dc536ce598
chao-xian <dev@kelvingan.co.uk>
2014-08-30 13:28:30 +0100

    Edit links added in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,20 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def show
